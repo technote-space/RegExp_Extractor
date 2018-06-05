dc77d4db4728e84059e25ec0ec1c9d2ea691038e
technote <technote.space@gmail.com>
2018-06-05 18:53:48 +0900

    first commit

---
+++ RegExp_Extractor/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace RegExp_Extractor
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.SuspendLayout();
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			this.openFileDialog1.Filter = "テキストファイル|*.txt";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(30, 30);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "ファイル選択";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(30, 104);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(214, 19);
+			this.textBox1.TabIndex = 1;
+			this.textBox1.Text = "https?://[^/]+/([^/]+)/([^/]+)/([^/]+)";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(30, 86);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(53, 12);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "正規表現";
+			// 
+			// button2
+			// 
+			this.button2.Enabled = false;
+			this.button2.Location = new System.Drawing.Point(32, 208);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 4;
+			this.button2.Text = "出力";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// saveFileDialog1
+			// 
+			this.saveFileDialog1.Filter = "CSVファイル|*.csv";
+			// 
+			// listView1
+			// 
+			this.listView1.Location = new System.Drawing.Point(278, 13);
+			this.listView1.Name = "listView1";
+			this.listView1.OwnerDraw = true;
+			this.listView1.Size = new System.Drawing.Size(452, 388);
+			this.listView1.TabIndex = 5;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			this.listView1.View = System.Windows.Forms.View.Details;
+			this.listView1.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.listView1_ColumnClick);
+			this.listView1.DrawColumnHeader += new System.Windows.Forms.DrawListViewColumnHeaderEventHandler(this.listView1_DrawColumnHeader);
+			this.listView1.DrawItem += new System.Windows.Forms.DrawListViewItemEventHandler(this.listView1_DrawItem);
+			this.listView1.DrawSubItem += new System.Windows.Forms.DrawListViewSubItemEventHandler(this.listView1_DrawSubItem);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(742, 413);
+			this.Controls.Add(this.listView1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "Form1";
+			this.Text = "抽出ツール";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+		private System.Windows.Forms.ListView listView1;
+	}
+}
+
---
+++ RegExp_Extractor/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace RegExp_Extractor
+{
+	public partial class Form1 : Form
+	{
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+
+			if (textBox1.Text.Length == 0)
+			{
+				MessageBox.Show("please input regular pattern");
+				return;
+			}
+
+			if (openFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+				listView1.Columns.Clear();
+				listView1.Items.Clear();
+				bool first = true;
+				foreach (GroupCollection item in this.match(openFileDialog1.FileName, textBox1.Text))
+				{
+					var items = item.Cast<Group>().Select(x => x.ToString());
+
+					if (first)
+					{
+						first = false;
+						var headers = items.Select(x =>
+						{
+							var c = new ColumnHeader();
+							c.Tag = true;
+							return c;
+						});
+						listView1.Columns.AddRange(headers.ToArray());
+					}
+
+					listView1.Items.Add(new ListViewItem(items.ToArray()));
+				}
+				button2.Enabled = listView1.Items.Count > 0;
+			}
+		}
+
+		private IEnumerable<GroupCollection> match(string file, string reg)
+		{
+			using (var sr = new StreamReader(openFileDialog1.FileName))
+			{
+				while (!sr.EndOfStream)
+				{
+					var line = sr.ReadLine();
+					var match = Regex.Match(line, textBox1.Text);
+					if (match.Success)
+					{
+						yield return match.Groups;
+					}
+				}
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+				using (var sw = new StreamWriter(saveFileDialog1.FileName, false, Encoding.GetEncoding("Shift_JIS")))
+				{
+					int index = 0;
+					bool[] include = new bool[listView1.Columns.Count];
+					foreach (ColumnHeader item in listView1.Columns)
+					{
+						include[index++] = Convert.ToBoolean(item.Tag);
+					}
+					foreach (ListViewItem item in listView1.Items)
+					{
+						index = 0;
+						var data = new List<string>();
+						foreach (ListViewItem.ListViewSubItem i in item.SubItems)
+						{
+							if (index < include.Length && include[index])
+							{
+								data.Add(i.Text);
+							}
+							index++;
+						}
+						sw.Write(string.Join(",", data));
+						sw.Write("\r\n");
+					}
+				}
+			}
+
+		}
+
+		private void listView1_DrawColumnHeader(object sender, DrawListViewColumnHeaderEventArgs e)
+		{
+			e.DrawBackground();
+			bool value = Convert.ToBoolean(e.Header.Tag);
+			CheckBoxRenderer.DrawCheckBox(e.Graphics,
+				new Point(e.Bounds.Left + 4, e.Bounds.Top + 4),
+				value ? System.Windows.Forms.VisualStyles.CheckBoxState.CheckedNormal :
+				System.Windows.Forms.VisualStyles.CheckBoxState.UncheckedNormal);
+		}
+
+		private void listView1_DrawItem(object sender, DrawListViewItemEventArgs e)
+		{
+			e.DrawDefault = true;
+		}
+
+		private void listView1_DrawSubItem(object sender, DrawListViewSubItemEventArgs e)
+		{
+			e.DrawDefault = true;
+		}
+
+		private void listView1_ColumnClick(object sender, ColumnClickEventArgs e)
+		{
+			bool value = Convert.ToBoolean(this.listView1.Columns[e.Column].Tag);
+			this.listView1.Columns[e.Column].Tag = !value;
+			this.listView1.Invalidate();
+		}
+	}
+}
